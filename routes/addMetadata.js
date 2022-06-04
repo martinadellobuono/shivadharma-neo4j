542ff2a5092540765c67e9b86b1b88658ae754cf
Martina Dello Buono <martina.dellobuono2@unibo.it>
2022-06-04 22:55:25 +0200

    update

--- routes/addMetadata.js
+++ routes/addMetadata.js
@@ -39,7 +39,7 @@ router.post("/addMetadata/:id", async (req, res) => {
     const session = driver.session();
     try {
         const data = await session.writeTransaction(tx => tx
-            .run(`MATCH (edition:Edition)-[e:EDITED_BY]->(editor:Editor) MATCH (work:Work)-[w:WRITTEN_BY]->(author:Author) WHERE id(edition) = ${idEdition} AND id(editor) = ${idEditor} MERGE (edition)-[p:PUBLISHED_ON]->(date:Date {date: $date}) MERGE (date)-[ed:EDITION_BY]->(editor) RETURN edition.title, author.name, editor.name, date.date`, {date: req.body.date})                
+            .run(`MATCH (edition:Edition)-[e:EDITED_BY]->(editor:Editor) WHERE id(edition) = ${idEdition} AND id(editor) = ${idEditor} MATCH (work:Work)-[w:WRITTEN_BY]->(author:Author) SET author.name = "${req.body.author}" SET edition.title = "${req.body.title}" SET editor.name = "${req.body.editor}" MERGE (edition)-[p:PUBLISHED_ON]->(date {on: $date}) RETURN edition.title, editor.name, author.name, date.on`, {date: req.body.date})                
         );
         const title = data.records[0]["_fields"][0]
         const author = data.records[0]["_fields"][1]
@@ -58,5 +58,35 @@ router.post("/addMetadata/:id", async (req, res) => {
         await session.close();
     };
 });
+
+/*router.post("/addMetadata/:id", async (req, res) => {
+    var idEdition = req.params.id.split("/").pop().split("-")[0];
+    var idEditor = req.params.id.split("/").pop().split("-")[1];
+    const session = driver.session();
+    try {
+        const data = await session.writeTransaction(tx => tx
+            .run(`MATCH (edition:Edition)-[e:EDITED_BY]->(editor:Editor) WHERE id(edition) = ${idEdition} AND id(editor) = ${idEditor} MATCH (work:Work)-[w:WRITTEN_BY]->(author:Author) SET author.name = "${req.body.author}" SET edition.title = "${req.body.title}" SET editor.name = "${req.body.editor}" RETURN edition.title, editor.name, author.name`)                
+        );
+        const title = data.records[0]["_fields"][0]
+        const author = data.records[0]["_fields"][1]
+        const editor = data.records[0]["_fields"][2]
+        //const date = data.records[0]["_fields"][3]
+
+        res.send(title + author + editor);
+
+        res.render("addAnnotations", {
+            id: req.params.id,
+            title: title,
+            author: author,
+            editor: editor,
+            date: date
+        });
+
+    } catch (err) {
+        console.log("Error related to Neo4j: " + err);
+    } finally {
+        await session.close();
+    };
+});*/
 
 module.exports = router;
