1ead6ea6cf4ea37b8b536fe0b15507fe45324495
Martina Dello Buono <martina.dellobuono2@unibo.it>
2022-06-05 10:21:14 +0200

    control-file

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const path = require("path");
-const formidable = require("formidable");
 
 var app = express();
-
-app.use(express.static("public"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
